7037a61bab6c3d85032d031079f560f115d13f53
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-10-16 20:34:27 -0400

    Added inferred type for moveset

--- src/data/InferredTypes.ts
+++ src/data/InferredTypes.ts
@@ -1,7 +1,9 @@
-import { mockPokemonResponse } from "../mocks/pokemon.mock";
-import { mockSpeciesResponse } from "./../mocks/species.mock";
-import { mockListResponse } from "./../mocks/listPokemon.mock";
+import { mockPokemonResponse } from "../mocks/data/pokemon.mock";
+import { mockSpeciesResponse } from "../mocks/data/species.mock";
+import { mockListResponse } from "../mocks/data/listPokemon.mock";
+import { mockMove } from "./../mocks/data/move.mock";
 
 export type PokemonInferredType = typeof mockPokemonResponse;
 export type PokemonSpeciesInferredType = typeof mockSpeciesResponse;
 export type PokemonListInferredType = typeof mockListResponse;
+export type PokemonMoveInferredType = typeof mockMove;
