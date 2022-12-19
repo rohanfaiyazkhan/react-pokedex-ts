eea783cb94f29c6b83e9de678132c75fe2fc3585
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-12-19 12:32:03 -0500

    Refactored main tag to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import { BrowserRouter } from "react-router-dom";
 import ContextWrappers from "./contexts/ContextWrappers";
 import AppRouter from "./router/Routes";
 
+const Main: React.FC<React.PropsWithChildren> = (props) => {
+    return (
+        <main className="w-full h-full mx-auto min-h-screen xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+            {props.children}
+        </main>
+    );
+};
+
 const App: React.FC = () => {
     return (
         <BrowserRouter>
-            <div className="w-full h-full mx-auto min-h-screen xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+            <Main>
                 <ContextWrappers>
                     <AppRouter />
                 </ContextWrappers>
-            </div>
+            </Main>
         </BrowserRouter>
     );
 };
