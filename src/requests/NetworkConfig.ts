a3f6710469a9c93407e7966527994b64bc6c2cbf
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-12-22 22:55:49 -0500

    Default stale time set, and number of items per page reduced to fit elements in one page

--- src/requests/NetworkConfig.ts
+++ src/requests/NetworkConfig.ts
@@ -1,7 +1,7 @@
 export const BASE_API_URL = "https://pokeapi.co/api/v2";
-export const StalenessTimeoutInSeconds = Object.freeze({
-    DEFAULT: 300,
+export const StalenessTimeoutInMs = Object.freeze({
+    DEFAULT: 300_000,
 });
 export const NumberOfItemsPerPage = {
-    DEFAULT: 50,
+    DEFAULT: 24,
 };
--- src/requests/queryClient.ts
+++ src/requests/queryClient.ts
@@ -1,10 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
+import { StalenessTimeoutInMs } from "./NetworkConfig";
 
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             refetchOnWindowFocus: false,
             refetchOnMount: false,
+            staleTime: StalenessTimeoutInMs.DEFAULT,
             retry: false,
         },
     },
