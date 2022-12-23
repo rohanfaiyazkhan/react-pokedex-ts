2b0b9c729eb671ba7ef58472ea048bcdf07a9c6c
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-12-22 21:39:01 -0500

    Types styling change

--- src/components/IndividualView/PokemonTypes.tsx
+++ src/components/IndividualView/PokemonTypes.tsx
@@ -19,25 +19,25 @@ const PokemonTypes: React.FC<IPokemonTypesProps> = ({
 
     return (
         <div className={className} style={style}>
-            <p className="mb-2 text-sm">Type</p>
-            <p>
+            <p className="mb-2 text-sm font-bold font-heading">Type</p>
+            <div className="flex items-center space-x-2">
                 {types.map(({ type }, idx) => {
                     const typeStyles = getPokemonTypeColorClassNames(type.name);
 
                     return (
-                        <span
+                        <p
                             key={`type-${idx}-${type.name}`}
                             className={combineClassnames(
-                                "px-2 py-1 font-bold mr-2 uppercase border border-gray-500 rounded-sm",
                                 typeStyles?.bg,
-                                typeStyles?.text
+                                typeStyles?.text,
+                                "px-2 py-1 font-bold mr-2 uppercase border border-current rounded-sm"
                             )}
                         >
                             {type.name}
-                        </span>
+                        </p>
                     );
                 })}
-            </p>
+            </div>
         </div>
     );
 };
