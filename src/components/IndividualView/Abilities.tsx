eca85bf287249ced7d065115d49160bdcc6f4bac
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-12-22 21:37:21 -0500

    Abilities styling changed

--- src/components/IndividualView/Abilities.tsx
+++ src/components/IndividualView/Abilities.tsx
@@ -15,16 +15,22 @@ const Abilities: React.FC<IAbilitiesProps> = ({
 
     return (
         <div className={className} style={style}>
-            <p className="text-sm">Abilities</p>
-            {abilities.map((ability, idx) => (
-                <p key={`ability-${ability.ability.name}-${idx}`}>
-                    {idx + 1}.{" "}
-                    <span className="capitalize">{ability.ability.name}</span>
-                    {ability.is_hidden && (
-                        <span className="ml-2 italic text-sm">(Hidden)</span>
-                    )}
-                </p>
-            ))}
+            <p className="font-bold mb-1 font-heading">Abilities</p>
+            <ol>
+                {abilities.map((ability, idx) => (
+                    <li key={`ability-${ability.ability.name}-${idx}`}>
+                        <span className="text-xs mr-2">{idx + 1}.</span>
+                        <span className="capitalize">
+                            {ability.ability.name}
+                        </span>
+                        {ability.is_hidden && (
+                            <span className="ml-1 italic text-sm">
+                                (Hidden)
+                            </span>
+                        )}
+                    </li>
+                ))}
+            </ol>
         </div>
     );
 };
