b0af6e2f6da06188e1d262c8acc11c9f3c8cd213
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2023-01-03 20:48:23 -0500

    Fixed caching not working by adding a cache timeout

--- src/requests/NetworkConfig.ts
+++ src/requests/NetworkConfig.ts
@@ -1,7 +1,5 @@
 export const BASE_API_URL = "https://pokeapi.co/api/v2";
-export const StalenessTimeoutInMs = Object.freeze({
-    DEFAULT: 300_000,
-});
+
 export const NumberOfItemsPerPage = {
     DEFAULT: 24,
 };
--- src/requests/queryClient.ts
+++ src/requests/queryClient.ts
@@ -1,12 +1,15 @@
 import { QueryClient } from "@tanstack/react-query";
-import { StalenessTimeoutInMs } from "./NetworkConfig";
+
+const CACHE_TIMEOUT_MS = 300_000;
+const STALE_TIMEOUT_MS = 240_000;
 
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             refetchOnWindowFocus: false,
             refetchOnMount: false,
-            staleTime: StalenessTimeoutInMs.DEFAULT,
+            staleTime: CACHE_TIMEOUT_MS,
+            cacheTime: STALE_TIMEOUT_MS,
             retry: false,
         },
     },
