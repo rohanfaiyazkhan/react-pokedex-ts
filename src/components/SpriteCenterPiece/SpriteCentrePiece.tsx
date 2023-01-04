30a2cf5318e8903a46d3335cb8a6d934cf211280
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2023-01-03 20:51:58 -0500

    Added a mobile view for SpriteCentrePiece

--- src/components/SpriteCenterPiece/SpriteCentrePiece.tsx
+++ src/components/SpriteCenterPiece/SpriteCentrePiece.tsx
@@ -19,15 +19,20 @@ const SpriteCentrePiece: React.FC<SpriteCentrePieceProps> = ({
         shiny: false,
     });
 
-    let rootClassName = combineClassnames(
+    const mobileImageClassnames = combineClassnames(
         className,
-        "relative rounded-full bg-red-50 border border-red-400 shadow-inner"
+        "xl:hidden w-full h-full object-contain"
+    );
+
+    const roundedContainerClassnames = combineClassnames(
+        className,
+        "hidden xl:block relative xl:rounded-full bg-red-50 border border-red-400 shadow-inner"
     );
 
     return (
-        <div className={rootClassName} style={style}>
+        <>
             <img
-                className="w-full h-full object-cover"
+                className={mobileImageClassnames}
                 style={{
                     minWidth: 80,
                     minHeight: 80,
@@ -35,7 +40,18 @@ const SpriteCentrePiece: React.FC<SpriteCentrePieceProps> = ({
                 src={spriteUrl.toString()}
                 alt={"Sprite of pokemon " + (pokemonName ?? pokemonId)}
             />
-        </div>
+            <div className={roundedContainerClassnames} style={style}>
+                <img
+                    className="w-full h-full object-cover"
+                    style={{
+                        minWidth: 80,
+                        minHeight: 80,
+                    }}
+                    src={spriteUrl.toString()}
+                    alt={"Sprite of pokemon " + (pokemonName ?? pokemonId)}
+                />
+            </div>
+        </>
     );
 };
 
