1e318832f1b3dbd976b026314abcf150018cf426
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2023-01-06 23:54:33 -0500

    Fixed change event not firing in searchbar test

--- src/tests/components/Searchbar.test.tsx
+++ src/tests/components/Searchbar.test.tsx
@@ -1,10 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { SearchBarElementIds } from "./../../components/SearchBar/SearchBarElementIds";
+import { MemoryRouter } from "react-router";
 
 test("Search bar component", async () => {
-    render(<SearchBar />);
+    render(
+        <MemoryRouter>
+            <SearchBar />
+        </MemoryRouter>
+    );
 
     const overlay = screen.getByTestId(SearchBarElementIds.ScreenOverlay);
     const dropdown = screen.getByRole("listbox");
@@ -23,9 +28,13 @@ test("Search bar component", async () => {
     expect(overlay).toHaveClass("animation-overlay-fade-in");
     expect(dropdown).toHaveClass("invisible");
 
-    userEvent.keyboard("bul");
+    fireEvent.change(input, {
+        target: { value: "bul" },
+    });
 
     expect(overlay).toHaveClass("animation-overlay-fade-in");
     expect(dropdown).not.toHaveClass("invisible");
     expect(screen.getByText(/loading/i)).toBeInTheDocument();
+
+    await waitFor(() => expect(dropdown.childElementCount).toBeGreaterThan(1));
 });
